f44e87123a75bd04ed666fef930f4e481478386a
ktaiga <ktaiga@gmail.com>
2018-09-03 00:53:55 -0700

    wtf

---
+++ demo_ubuntu/bootstrap_python.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y python python-pip python-dev build-essential
+sudo pip install --upgrade pip
+sudo pip install --upgrade virtualenv
+sudo apt-get install -y python3 python3-pip python3-dev
+sudo pip3 install --upgrade pip
+sudo pip3 install --upgrade virtualenv
