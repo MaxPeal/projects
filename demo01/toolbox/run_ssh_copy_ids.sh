be4c593711faa7c792d962fe42281f72f425424e
ktaiga <ktaiga@gmail.com>
2018-09-25 16:28:04 +0000

    wtf

--- demo01/toolbox/run_ssh_copy_ids.sh
+++ demo01/toolbox/run_ssh_copy_ids.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sshpass -p 'vagrant' ssh-copy-id -o 'StrictHostKeyChecking no' vagrant@control
-sshpass -p 'vagrant' ssh-copy-id -o 'StrictHostKeyChecking no' vagrant@lb01
-sshpass -p 'vagrant' ssh-copy-id -o 'StrictHostKeyChecking no' vagrant@app01
-sshpass -p 'vagrant' ssh-copy-id -o 'StrictHostKeyChecking no' vagrant@app02
-sshpass -p 'vagrant' ssh-copy-id -o 'StrictHostKeyChecking no' vagrant@db01
+sshpass -p 'vagrant' ssh-copy-id -o 'StrictHostKeyChecking no' vagrant@nat01
+sshpass -p 'vagrant' ssh-copy-id -o 'StrictHostKeyChecking no' vagrant@dnsmasq01
+sshpass -p 'vagrant' ssh-copy-id -o 'StrictHostKeyChecking no' vagrant@client01
+sshpass -p 'vagrant' ssh-copy-id -o 'StrictHostKeyChecking no' vagrant@client02
