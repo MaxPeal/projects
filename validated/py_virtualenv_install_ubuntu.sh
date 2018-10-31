e734f91b17b43b016066688bc13eb50eb72745b0
ktaiga <ktaiga@gmail.com>
2018-10-31 08:18:27 +0000

    wtf

--- validated/py_virtualenv_install_ubuntu.sh
+++ validated/py_virtualenv_install_ubuntu.sh
@@ -2,7 +2,8 @@
 
 sudo apt-get update
 sudo apt-get install python3-pip
-sudo pip3 install virtualenv
+sudo -H pip3 install virtualenv
+sudo -H pip3 install --upgrade pip
 
 # to create virtual environment
 #virtualenv venv
@@ -15,3 +16,13 @@ sudo pip3 install virtualenv
 # to deactivate
 #deactivate
 
+virtualenv -p python3 venv
+. venv/bin/activate
+
+pip install Flask
+pip install sqlalchemy
+
+deactivate
+
+#TODO: try pip install -r requirements.txt
+# reference: https://stackoverflow.com/questions/7225900/how-to-install-packages-using-pip-according-to-the-requirements-txt-file-from-a
