a453b4df5ae854eaca10e1b958a49c0ebf8a08af
ktaiga <ktaiga@gmail.com>
2018-10-30 18:15:48 -0700

    wtf

--- validated/mariadb_install_ubuntu.sh
+++ validated/mariadb_install_ubuntu.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 PASSWORD=Super123
+DBADMIN=mariadb
+#DBHOST=localhost
+DBHOST=% # for remote user
 export DEBIAN_FRONTEND="noninteractive"
 #sudo debconf-set-selections <<< "mariadb-server mysql-server/root_password password $PASSWORD"
 #sudo debconf-set-selections <<< "mariadb-server mysql-server/root_password_again password $PASSWORD" 
@@ -8,3 +11,10 @@ export DEBIAN_FRONTEND="noninteractive"
 echo "mariadb-server mysql-server/root_password password $PASSWORD" | sudo debconf-set-selections
 echo "mariadb-server mysql-server/root_password_again password $PASSWORD" | sudo debconf-set-selections
 sudo apt-get install -y mariadb-server python-mysqldb
+
+# create new root user (root not longer allow login via phpmyadmin...)
+#sudo mysql -uroot -p${PASSWORD} -e "DROP USER IF EXISTS '${DBADMIN}'@'${DBHOST}';"
+sudo mysql -uroot -p${PASSWORD} -e "DROP USER '${DBADMIN}'@'${DBHOST}';"
+sudo mysql -uroot -p${PASSWORD} -e "CREATE USER '${DBADMIN}'@'${DBHOST}' IDENTIFIED BY '${PASSWORD}';"
+sudo mysql -uroot -p${PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO '${DBADMIN}'@'${DBHOST}' WITH GRANT OPTION;"
+sudo mysql -uroot -p${PASSWORD} -e "FLUSH PRIVILEGES;"
