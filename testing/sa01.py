994c9482ed93943d32d3a9f911570df04043eae6
ktaiga <ktaiga@coco-THINK>
2018-11-02 16:10:14 -0700

    wtf

--- testing/sa01.py
+++ testing/sa01.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, create_engine
+engine = create_engine('mysql+pymysql://mariadb:Super123@192.168.56.101/mydatabase')
+metadata = MetaData()
+users = Table( 'users', metadata,
+    Column( name='id', type=Integer, primary_key=True),
+    Column( name='name', type=String),
+    Column( name='fullname', type=String),
+)
+addresses = Table( 'addresses', metadata,
+    Column( name='id', type=Integer, primary_key=True),
+    Column( name='user_id', type=None, ForeignKey('users.id')),
+    Column( name='email_address', type=String, nullable=False),
+)
 
-import yaml
-
-
-with open("sa01.yml", "r") as stream:
-	
-	s = yaml.load(stream)
-print(s["tables"][0])
-
+metadata.create_all(engine)
