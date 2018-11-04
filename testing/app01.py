967f055f561d0a4b44f9cbd174d9a03de2471a45
ktaiga <ktaiga@app01>
2018-11-04 10:02:31 +0000

    wtf

--- testing/app01.py
+++ testing/app01.py
@@ -1,4 +1,5 @@
+import sys
+sys.path.append('/home/vagrant/projects')
 from testing.sa01_dml import users, users_select, conn, engine
-
 # conn = engine.connect()
 print(conn.execute(users_select).fetchall())
