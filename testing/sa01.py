6d4a0a95e2151af824936109446189adca2bcbca
ktaiga <ktaiga@app01>
2018-11-04 00:03:39 +0000

    wtf

--- testing/sa01.py
+++ testing/sa01.py
@@ -1,17 +1,28 @@
-# -*- coding: utf-8 -*-
-
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, create_engine
 engine = create_engine('mysql+pymysql://mariadb:Super123@192.168.56.101/mydatabase')
 metadata = MetaData()
+
 users = Table( 'users', metadata,
-    Column( name='id', type_=Integer, primary_key=True),
-    Column( name='name', type_=String(50)),
-    Column( name='fullname', type_=String(100)),
+    Column('id', Integer, 
+        nullable=False,
+        autoincrement=True,
+        primary_key=True,
+        index=True,
+    ),
+    Column('name', String(50), 
+    ),
+    Column('fullname', String(100), 
+    ),
 )
 addresses = Table( 'addresses', metadata,
-    Column( name='id', type_=Integer, primary_key=True),
-    Column( 'user_id', None, ForeignKey('users.id')),
-    Column( name='email_address', type_=String(200), nullable=False),
+    Column('id', Integer, 
+        primary_key=True,
+    ),
+    Column('user_id', None, 
+        ForeignKey('users.id'),
+    ),
+    Column('email_address', String(200), 
+    ),
 )
 
 metadata.create_all(engine)
--- testing/sa_create.py
+++ testing/sa_create.py
@@ -1,18 +1,20 @@
-#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import jinja2
 import yaml
 
 with open("sa01.yml", "r") as stream:
-	
     s = yaml.load(stream)
 
 TEMPLATE_DIR = "./"
 TEMPLATE_FILE = "sa01.j2"
+OUTPUT_FILE = "sa01.py"
 templateLoader = jinja2.FileSystemLoader(searchpath=TEMPLATE_DIR)
-templateEnv = jinja2.Environment(loader=templateLoader)
+templateEnv = jinja2.Environment(loader=templateLoader, trim_blocks=True, lstrip_blocks=True)
 template = templateEnv.get_template(TEMPLATE_FILE)
 outputText = template.render(tables=s["tables"]) # this is where to put args to the template renderer
+
+with open(OUTPUT_FILE, "w") as f:
+    f.write(outputText)
 
 print(outputText)
