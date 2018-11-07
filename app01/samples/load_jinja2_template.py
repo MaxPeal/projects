4e42108142fc71d468b25553b02316e2b318849e
ktaiga <ktaiga@coco-THINK>
2018-11-06 16:48:37 -0800

    wtf

--- app01/samples/load_jinja2_template.py
+++ app01/samples/load_jinja2_template.py
@@ -10,12 +10,13 @@ OUTPUT_FILE = "test.py"
 with open(SCHEMA_FILE, "r", encoding="utf-8") as f:
     s = yaml.load(f)
 
-
+# loading jinja2 template
 templateLoader = jinja2.FileSystemLoader(searchpath=TEMPLATE_DIR)
 templateEnv = jinja2.Environment(loader=templateLoader, trim_blocks=True, lstrip_blocks=True)
 template = templateEnv.get_template(TEMPLATE_FILE)
 renderedText = template.render(database=s["database"]) # this is where to put args to the template renderer
 
+# writing rendered output
 with open(OUTPUT_FILE, "w") as f:
     f.write(renderedText)
 print(renderedText)
