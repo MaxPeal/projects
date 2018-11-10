c1668da925ca62b0f7c51cfd55bfacb20c6c501e
ktaiga <ktaiga@COCO>
2018-11-10 06:52:49 -0800

    wtf

--- app01/samples/load_jinja2_template.py
+++ app01/samples/load_jinja2_template.py
@@ -2,10 +2,12 @@
 import jinja2
 import yaml
 
-SCHEMA_FILE="test.yml"
+SCHEMA_FILE="test_schema.yml"
 TEMPLATE_DIR = "./"
-TEMPLATE_FILE = "test.j2"
-OUTPUT_FILE = "test.py"
+TEMPLATE_FILE_DB_INIT = "test_schema_db_init.j2"
+OUTPUT_FILE_DB_INIT = "test_schema_db_init.sh"
+TEMPLATE_FILE_TABLE_INIT = "test_schema_table_init.j2"
+OUTPUT_FILE_TABLE_INIT = "test_schema_table_init.py"
 # loading yaml file
 with open(SCHEMA_FILE, "r", encoding="utf-8") as f:
     s = yaml.load(f)
@@ -13,9 +15,17 @@ with open(SCHEMA_FILE, "r", encoding="utf-8") as f:
 
 templateLoader = jinja2.FileSystemLoader(searchpath=TEMPLATE_DIR)
 templateEnv = jinja2.Environment(loader=templateLoader, trim_blocks=True, lstrip_blocks=True)
-template = templateEnv.get_template(TEMPLATE_FILE)
-renderedText = template.render(database=s["database"]) # this is where to put args to the template renderer
 
-with open(OUTPUT_FILE, "w") as f:
+# generating db init script
+template = templateEnv.get_template(TEMPLATE_FILE_DB_INIT)
+renderedText = template.render(host=s["host"])
+with open(OUTPUT_FILE_DB_INIT, "w") as f:
+    f.write(renderedText)
+print(renderedText)
+
+# generating table init script
+template = templateEnv.get_template(TEMPLATE_FILE_TABLE_INIT)
+renderedText = template.render(host=s["host"]) # this is where to put args to the template renderer
+with open(OUTPUT_FILE_TABLE_INIT, "w") as f:
     f.write(renderedText)
 print(renderedText)
