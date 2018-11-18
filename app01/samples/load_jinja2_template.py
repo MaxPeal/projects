0700a8d18c6bcbb4420af188fa507c23815a5714
ktaiga <ktaiga@coco-THINK>
2018-11-18 11:42:36 -0800

    wtf

--- app01/samples/load_jinja2_template.py
+++ app01/samples/load_jinja2_template.py
@@ -6,8 +6,12 @@ SCHEMA_FILE="test_schema.yml"
 TEMPLATE_DIR = "./"
 TEMPLATE_FILE_DB_CREATE = "test_schema_db_create.j2"
 OUTPUT_FILE_DB_CREATE = "test_schema_db_create.sh"
-TEMPLATE_FILE_TABLE_CREATE = "test_schema_table_create.j2"
-OUTPUT_FILE_TABLE_CREATE = "test_schema_table_create.py"
+#TEMPLATE_FILE_TABLE_CREATE = "test_schema_table_create.j2"
+#OUTPUT_FILE_TABLE_CREATE = "test_schema_table_create.py"
+TEMPLATE_FILE_SQLAORM_DB_CONNECT = "test_schema_sqlaorm_db_connect.j2"
+OUTPUT_FILE_SQLAORM_DB_CONNECT = "test_schema_sqlaorm_db_connect.py"
+TEMPLATE_FILE_SQLAORM_TABLE_CREATE = "test_schema_sqlaorm_table_create.j2"
+OUTPUT_FILE_SQLAORM_TABLE_CREATE = "test_schema_sqlaorm_table_create.py"
 # loading yaml file
 with open(SCHEMA_FILE, "r", encoding="utf-8") as f:
     s = yaml.load(f)
@@ -24,8 +28,22 @@ with open(OUTPUT_FILE_DB_CREATE, "w") as f:
 print(renderedText)
 
 # generating table init script
-template = templateEnv.get_template(TEMPLATE_FILE_TABLE_CREATE)
+#template = templateEnv.get_template(TEMPLATE_FILE_TABLE_CREATE)
+#renderedText = template.render(host=s["host"]) # this is where to put args to the template renderer
+#with open(OUTPUT_FILE_TABLE_CREATE, "w") as f:
+#    f.write(renderedText)
+#print(renderedText)
+
+# generating script
+template = templateEnv.get_template(TEMPLATE_FILE_SQLAORM_DB_CONNECT)
 renderedText = template.render(host=s["host"]) # this is where to put args to the template renderer
-with open(OUTPUT_FILE_TABLE_CREATE, "w") as f:
+with open(OUTPUT_FILE_SQLAORM_DB_CONNECT, "w") as f:
+    f.write(renderedText)
+print(renderedText)
+
+# generating script
+template = templateEnv.get_template(TEMPLATE_FILE_SQLAORM_TABLE_CREATE)
+renderedText = template.render(host=s["host"]) # this is where to put args to the template renderer
+with open(OUTPUT_FILE_SQLAORM_TABLE_CREATE, "w") as f:
     f.write(renderedText)
 print(renderedText)
